56d0dc85a03df94da21f95f7e993af6f3a7e484c
minirop <minirop@hotmail.com>
2026-01-26 23:21:05 +0100

    Splitted enums

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use crate::lvl_setup::Category;
-use byteorder::BigEndian;
-use byteorder::ReadBytesExt;
-use enums::ActorId;
-use rand::prelude::SliceRandom;
-use rand::rng;
+#![allow(unused)]
+use crate::enums::*;
+use db360::ASSETS;
+use std::error::Error;
 use std::fs::OpenOptions;
-use std::io::Seek;
-use std::{error::Error, io::SeekFrom};
 
+mod db360;
 mod dialogue;
 mod enums;
 mod lvl_setup;
@@ -21,52 +18,42 @@ fn main() -> Result<(), Box<dyn Error>> {
         .read(true)
         .write(true)
         .open("db360.cmp")?;
-    // 1821 = SM lvl setup (+1 for "number of elements")
-    file.seek(SeekFrom::Start(1822 * 8))?;
-    let addr = file.read_u32::<BigEndian>()?;
-    file.seek(SeekFrom::Start(addr as u64 + 3702 * 8))?;
-    let mut setup = lvl_setup::LevelSetup::new(&mut file)?;
 
-    let mut objects = vec![];
-
-    // an example how you can shuffle the honeycombs and extralives in SM
-    for c in &setup.cubes {
-        for o in &c.props_1 {
-            if let Category::Actor(actor_id) = o.category {
-                match actor_id {
-                    ActorId::ExtraLife | ActorId::HoneyComb => objects.push(*o),
-                    _ => break,
-                }
+    let mut next_id = 0u16;
+    for asset in ASSETS {
+        match asset {
+            AssetId::Empty => {}
+            AssetId::Animation(animation_id) => {
+                assert_eq!(next_id, animation_id.into());
+            }
+            AssetId::Midi(midi_id) => {
+                assert_eq!(next_id, midi_id.into());
+            }
+            AssetId::Model(model_id) => {
+                assert_eq!(next_id, model_id.into());
+            }
+            AssetId::MapSetup(map_setup_id) => {
+                assert_eq!(next_id, map_setup_id.into());
+            }
+            AssetId::Dialogue(dialogue_id) => {
+                assert_eq!(next_id, dialogue_id.into());
+            }
+            AssetId::Credits(credits_id) => {
+                assert_eq!(next_id, credits_id.into());
+            }
+            AssetId::Sprite(sprite_id) => {
+                assert_eq!(next_id, sprite_id.into());
+            }
+            AssetId::Question(question_id) => {
+                assert_eq!(next_id, question_id.into());
+            }
+            AssetId::Unknown(unknown_id) => {
+                assert_eq!(next_id, unknown_id.into());
             }
         }
+        next_id += 1;
     }
-
-    objects.shuffle(&mut rng());
-
-    let mut index = 0;
-    for c in &mut setup.cubes {
-        for o in &mut c.props_1 {
-            if let Category::Actor(actor_id) = o.category {
-                match actor_id {
-                    ActorId::ExtraLife | ActorId::HoneyComb => {
-                        // overwrite everything except the position
-                        o.selector_or_radius = objects[index].selector_or_radius;
-                        o.category = objects[index].category;
-                        o.unk_bit_0 = objects[index].unk_bit_0;
-                        o.marker_id = objects[index].marker_id;
-                        o.byte_0b = objects[index].byte_0b;
-                        o.bitfield_0c = objects[index].bitfield_0c;
-                        o.bitfield_10 = objects[index].bitfield_10;
-                        index += 1;
-                    }
-                    _ => break,
-                }
-            }
-        }
-    }
-
-    file.seek(SeekFrom::Start(addr as u64 + 3702 * 8))?;
-    setup.write(&mut file)?;
+    assert_eq!(next_id, ASSETS.len() as u16);
 
     Ok(())
 }
